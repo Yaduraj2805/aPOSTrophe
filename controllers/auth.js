df709a5c87d89ac0d6a6ee9c5820bfe1d93224c9
Yaduraj2805 <80809010+Yaduraj2805@users.noreply.github.com>
2023-08-02 01:22:45 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,7 +24,7 @@ export const register = async (req, res) => {
       lastName,
       email,
       password: passwordHash,
-       picturePath,
+       picturePath:req.file.path,
       friends,
       location,
       occupation,
